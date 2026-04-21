8b4374c343c243a35206d827db1f772a162d35b7
bszczerba13 <bartekk29alk@gmail.com>
2026-04-22 00:10:42 +0200

    Sorting tests added

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,10 +1,16 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from pages.base_page import BasePage
 from pages.login_page import LoginPage
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Locators:
     SIGN_IN_LINK = (By.CSS_SELECTOR, "[data-test='nav-sign-in']")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "[data-test='product-price']")
+    SORT_LIST = (By.CSS_SELECTOR, "[data-test='sort']")
+    SORTING_COMPLETED = (By.CSS_SELECTOR, "[data-test='sorting_completed']")
 
 class HomePage(BasePage):
     """
@@ -14,3 +20,26 @@ class HomePage(BasePage):
     def click_sign_in(self):
         self.driver.find_element(*Locators.SIGN_IN_LINK).click()
         return LoginPage(self.driver)
+
+    def get_product_prices(self):
+        elements = self.driver.find_elements(*Locators.PRODUCT_PRICE)
+        prices = []
+        for element in elements:
+            price_value = element.text.replace('$', '').strip()
+            prices.append(float(price_value))
+        return prices
+
+    def sort_price_low_to_high(self):
+        sort_options = self.driver.find_element(*Locators.SORT_LIST)
+        select = Select(sort_options)
+        select.select_by_value('price,asc')
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(Locators.SORTING_COMPLETED))
+
+    def sort_price_high_to_low(self):
+        sort_options = self.driver.find_element(*Locators.SORT_LIST)
+        select = Select(sort_options)
+        select.select_by_value('price,desc')
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(Locators.SORTING_COMPLETED))
+
+    def _verify_page(self):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(Locators.SORT_LIST))
---
+++ tests/sorting_test.py
@@ -0,0 +1,16 @@
+from tests.base_test import BaseTest
+
+
+class SortingTest(BaseTest):
+    def setUp(self):
+        super().setUp()
+
+    def test_sort_price_low_to_high(self):
+        self.home_page.sort_price_low_to_high()
+        prices = self.home_page.get_product_prices()
+        self.assertEqual(prices, sorted(prices))
+
+    def test_sort_price_high_to_low(self):
+        self.home_page.sort_price_high_to_low()
+        prices = self.home_page.get_product_prices()
+        self.assertEqual(prices, sorted(prices, reverse=True))
